f4d0a07eda2d57aa570690ed31e7cb590db188ec
Alexis Vergara Quijada <31351178+DonMeguDev@users.noreply.github.com>
2024-02-19 17:55:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+//Elementos Nico
+const puntajeNico = document.getElementById('puntajeNico');
+const botonMenosNico = document.getElementById('botonMenosNico');
+const botonMasNico = document.getElementById('botonMasNico');
+const totalNico = document.getElementById('totalNico');
+const dolaresNico = document.getElementById('dolaresNico');
+const botonAgregarNico = document.getElementById('botonAgregarNico');
+
+//Elementos Fran
+const puntajeFran = document.getElementById('puntajeFran');
+const botonMenosFran = document.getElementById('botonMenosFran');
+const botonMasFran = document.getElementById('botonMasFran');
+const totalFran = document.getElementById('totalFran');
+const dolaresFran = document.getElementById('dolaresFran');
+const botonAgregarFran = document.getElementById('botonAgregarFran');
+
+
+let pNico = 0;
+let pFran = 0;
+let dNico = 0;
+let dFran = 0;
+
+botonMenosNico.addEventListener('click', () => {
+    if(pNico > 0){
+        pNico--;
+    }
+    puntajeNico.innerHTML = pNico;
+});
+
+botonMasNico.addEventListener('click', () => {
+    pNico++;
+    puntajeNico.innerHTML = pNico;
+});
+
+botonMenosFran.addEventListener('click', () => {
+    if(pFran > 0){
+        pFran--;
+    }
+    puntajeFran.innerHTML = pFran;
+});
+
+botonMasFran.addEventListener('click', () => {
+    pFran++;
+    puntajeFran.innerHTML = pFran;
+});
+
+botonAgregarNico.addEventListener('click', () => {
+    if(dolaresNico.value > 0){
+        dNico = dNico + parseFloat(dolaresNico.value);
+        totalNico.innerHTML = 'Total: $' + formatoPesos(dNico.toFixed(2)) + ' CLP';
+    }
+    dolaresNico.value = '';
+});
+
+botonAgregarFran.addEventListener('click', () => {
+    if(dolaresFran.value > 0){
+        dFran = dFran + parseFloat(dolaresFran.value);
+        totalFran.innerHTML = 'Total: $' + formatoPesos(dFran.toFixed(2)) + ' CLP';
+    }
+    dolaresFran.value = '';
+});
+
+const formatoPesos = (number) => {
+    const exp = /(\d)(?=(\d{3})+(?!\d))/g;
+    const rep = '$1,';
+    let arr = number.toString().split('.');
+    arr[0] = arr[0].replace(exp,rep);
+    return arr[1] ? arr.join('.'): arr[0];
+  }
